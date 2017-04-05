b95c3d5b87f919b909cdead46dcbf358366412d4
Dan Feeney <daniel.feeney@colorado.edu>
2017-04-05 07:55:21 -0600

    Update and rename Treatment_response.r to Treatment_responsePlot.r

--- Treatment_responsePlot.r
+++ Treatment_responsePlot.r
@@ -1,6 +1,9 @@
 sp <- ggplot(as.data.frame(ROM.dat), aes(x= ROM.dat[,3], y =ROM.dat[,2], col= factor(ROM.dat[,1]), group = ROM.dat[,1])) + geom_point() + geom_line() +
+  ggtitle("The magical effects of balls") +
   theme_bw() +
-  theme(axis.line = element_line(colour = "black"),
+  theme(plot.title = element_text(face = 'bold'),
+        axis.line = element_line(colour = "black", size =2),
+        axis.text = element_text(size = 12),
         panel.background = element_blank()) 
-sp + theme(legend.position='none') + ylab("Change in range of motion") + xlab("Treatment") +
+sp + theme(legend.position='none') + ylab("Change in torque") + xlab("Treatment") +
   scale_x_continuous(breaks = c(1,2,3), labels=c("Control", "TENS", "Balls"))
